6f80907a6687367ef8c8141b5038b99bbb3cfdca
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-07-30 21:02:36 +0000

    add registration route

---
+++ routes/register.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    res.render("register");
+  });
+
+  router.post("/", (req, res) => {
+    res.redirect("/home"); //should redirect to home page with login,not sure the urls yet
+  });
+
+  return router;
+};
