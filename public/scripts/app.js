bd19dda6199d54c13e9cee326365fe7a35d35024
MT91-dev <tariqm1991@gmail.com>
2022-08-01 00:36:26 +0000

    Add functions for user input sanitization & object creation from api response data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,50 @@
 // Client facing scripts here
 
+/**************************************************** HELPER FUNCTIONS ************************************************************/
+//requiring helper functions from helpers.js
+// const { sanitizeMovieQuery, sanitizeBookAndAuthorQuery, apiMoviesQueryToObject, apiBooksQueryToObject } = require('./helpers');
+
+//function to sanitize the users input using regex into correct format for API use
+function sanitizeMovieQuery(userInput) {
+  const sanitizedMovieString = userInput.trim().replace(/[^a-zA-Z]/g, " ").trim().replace(/ /g, "%20");
+  return sanitizedMovieString
+}
+//function to sanitize the users book and author inputs using regex into correct format for API use
+function sanitizeBookAndAuthorQuery(userInput) {
+  const sanitizeBookOrAuthorString = userInput.trim().replace(/[^a-zA-Z]/g, " ").trim().replace(/ /g, "+");
+  return sanitizeBookOrAuthorString
+}
+//function to handle the movies user query and corresponding api request response data and convert into temporary object
+function apiMoviesQueryToObject(apiResponseData) {
+  const movieQueryObject = {
+    "TITLE": apiResponseData.Title,
+    "GENRE": apiResponseData.Genre,
+    "PLOT": apiResponseData.Plot,
+    "POSTER": apiResponseData.Poster,
+    "RATED": apiResponseData.Rated,
+    "IMDB RATING": apiResponseData.imdbRating,
+    "RELEASED": apiResponseData.Released,
+    "RUNTIME": apiResponseData.Runtime,
+    "TYPE": apiResponseData.Type,
+  };
+  return movieQueryObject;
+}
+//function to handle the books user query and corresponding api request response data and convert into temporary object
+function apiBooksQueryToObject(apiResponseData) {
+  const booksQueryObject = {
+    "TITLE": apiResponseData.items[0].volumeInfo.title,
+    "AUTHOR": apiResponseData.items[0].volumeInfo.authors,
+    "DESCRIPTION": apiResponseData.items[0].volumeInfo.description,
+    "THUMBNAIL": apiResponseData.items[0].volumeInfo.imageLinks.thumbnail,
+    "TYPE": apiResponseData.items[0].volumeInfo.printType,
+    "PUBLISHER": apiResponseData.items[0].volumeInfo.publisher,
+    "PUBLISH DATE": apiResponseData.items[0].volumeInfo.publishedDate,
+  };
+  return booksQueryObject;
+}
+/**************************************************** HELPER FUNCTIONS ************************************************************/
+
+/******************************************************* DOM EVENTS ***************************************************************/
 //Starting the client side javascript with the document.ready so that DOM is loaded and ready
 $(document).ready(function () {
 
@@ -17,34 +62,33 @@ $(document).ready(function () {
     //jQuery variable that takes the users movie query input and stores into variable
     const $movieInputFromUser = $('#movie-query').val();
 
-    //function to sanitize the users input into correct format for API use
-    function sanitizeMovieQuery(userInput) {
-      const sanitizedMovieString = userInput.trim().replace(/[^a-zA-Z]/g, " ").trim().replace(/ /g, "%20");
-      return sanitizedMovieString
-    }
-
     //storing the user input into new variable by running it as parameter in sanitize function
     const sanitizedMovieQuery = sanitizeMovieQuery($movieInputFromUser);
 
     // console.log($movieInputFromUser);
     // console.log(sanitizedMovieQuery);
 
-    const queryMovie = "the%20terminator";
+    // const queryMovie = "the%20terminator";
 
     //ajax request for the movies api called omdbapi, returns a series of statements with required information that is contained within the response object
     $.ajax({
       type: "GET",
       url: `https://www.omdbapi.com/?t=${sanitizedMovieQuery}&apikey=a6b04247`,
       success: function (data) {
-        console.log("TITLE", data.Title);
-        console.log("GENRE", data.Genre);
-        console.log("PLOT", data.Plot);
-        console.log("POSTER", data.Poster);
-        console.log("RATED", data.Rated);
-        console.log("IMDB RATING", data.imdbRating);
-        console.log("RELEASED", data.Released);
-        console.log("RUNTIME", data.Runtime);
-        console.log("TYPE", data.Type);
+        const currentMovieObject = apiMoviesQueryToObject(data);
+        console.log(currentMovieObject);
+        for (const [key, value] of Object.entries(currentMovieObject)) {
+          console.log(`${key}: ${value}`);
+        }
+        // console.log("TITLE", data.Title);
+        // console.log("GENRE", data.Genre);
+        // console.log("PLOT", data.Plot);
+        // console.log("POSTER", data.Poster);
+        // console.log("RATED", data.Rated);
+        // console.log("IMDB RATING", data.imdbRating);
+        // console.log("RELEASED", data.Released);
+        // console.log("RUNTIME", data.Runtime);
+        // console.log("TYPE", data.Type);
       },
     })
   })
@@ -86,12 +130,6 @@ $(document).ready(function () {
     const $bookInputFromUser = $('#book-query').val();
     const $authorInputFromUser = $('#author-query').val();
 
-    //function to sanitize the users book and author inputs into correct format for API use
-    function sanitizeBookAndAuthorQuery(userInput) {
-      const sanitizeBookOrAuthorString = userInput.trim().replace(/[^a-zA-Z]/g, " ").trim().replace(/ /g, "+");
-      return sanitizeBookOrAuthorString
-    }
-
     //storing the user inputs into new variables by running it as parameter in sanitize function
     const sanitizedBookQuery = sanitizeBookAndAuthorQuery($bookInputFromUser);
     const sanitizedAuthorQuery = sanitizeBookAndAuthorQuery($authorInputFromUser);
@@ -109,15 +147,20 @@ $(document).ready(function () {
       type: "GET",
       url: `https://www.googleapis.com/books/v1/volumes?q=${sanitizedBookQuery}+inauthor:${sanitizedAuthorQuery}&key=AIzaSyADHzbY7CBGfxvALFDuOC6R4OenddipLBM`,
       success: function (data) {
-        console.log("TITLE:", data.items[0].volumeInfo.title);
-        console.log("AUTHOR:", data.items[0].volumeInfo.authors);
-        console.log("DESCRIPTION:", data.items[0].volumeInfo.description);
-        console.log("THUMBNAIL:", data.items[0].volumeInfo.imageLinks.thumbnail);
-        console.log("TYPE:", data.items[0].volumeInfo.printType);
-        console.log("PUBLISHER:", data.items[0].volumeInfo.publisher);
-        console.log("PUBLISH DATE:", data.items[0].volumeInfo.publishedDate);
+        const currentBookObject = apiBooksQueryToObject(data);
+        console.log(currentBookObject);
+        for (const [key, value] of Object.entries(currentBookObject)) {
+          console.log(`${key}: ${value}`);
+        }
+        // console.log("TITLE:", data.items[0].volumeInfo.title);
+        // console.log("AUTHOR:", data.items[0].volumeInfo.authors);
+        // console.log("DESCRIPTION:", data.items[0].volumeInfo.description);
+        // console.log("THUMBNAIL:", data.items[0].volumeInfo.imageLinks.thumbnail);
+        // console.log("TYPE:", data.items[0].volumeInfo.printType);
+        // console.log("PUBLISHER:", data.items[0].volumeInfo.publisher);
+        // console.log("PUBLISH DATE:", data.items[0].volumeInfo.publishedDate);
       },
     })
   })
-
 });
+/******************************************************* DOM EVENTS ***************************************************************/
