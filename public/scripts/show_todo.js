9f7b08ebffbd237c4113a9c3c3d8bf933070530c
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-08-01 23:14:41 +0000

    add database example

--- public/scripts/show_todo.js
+++ public/scripts/show_todo.js
@@ -18,19 +18,22 @@ $(document).ready(function() {
     }
   };
 
-  const listContentExample1 = [{title: "book1" }, {title: "book2" }, {title: "book3" }, {title: "book4" }];
-  const listContentExample2 = [{title: "book1" }, {title: "book2" }, {title: "book3" }, {title: "book4" }];
-  const listContentExample3 = [{title: "book1" }, {title: "book2" }, {title: "book3" }, {title: "book4" }];
-  const listContentExample4 = [{title: "book1" }, {title: "book2" }, {title: "book3" }, {title: "book4" }];
+  const listContentBook = [{title: "book1" }, {title: "book2" }, {title: "book3" }, {title: "book4" }];
+  const listContentMovie= [{title: "movie1" }, {title: "movie2" }, {title: "movie3" }, {title: "movie4" }];
+  const listContentRestaurant = [{title: "restaurant1" }, {title: "restaurant2" }, {title: "restaurant3" }, {title: "restaurant4" }];
+  const listContentProduct = [{title: "product1" }, {title: "product2" }, {title: "product3" }, {title: "product4" }];
 
 
-  console.log(renderListContent(listContentExample1, "bookList"));
+  // console.log(renderListContent(listContentExample1, "bookList"));
 
   $.ajax({
     type: "GET",
     url: '/dashboard',
     success: (data) => {
-      renderListContent(listContentExample, "bookList");
+      renderListContent(listContentBook, "bookList");
+      renderListContent(listContentMovie, "movieList");
+      renderListContent(listContentRestaurant, "restaurantList");
+      renderListContent(listContentProduct, "productList");
     }
 
   });
