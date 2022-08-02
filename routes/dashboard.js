dc7c827741928dcf18cc948e61770e85b69ed058
MT91-dev <tariqm1991@gmail.com>
2022-08-02 17:53:47 +0000

    Add async/await functions on backend for handling api response

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,14 +4,22 @@ const request = require('request')
 const { sanitizeMovieQuery, sanitizeFoodBusinessQuery, sanitizeBookAndAuthorQuery } = require('../public/scripts/helpers')
 const objdata = {}
 
+let apiResponseMoviesRaw
+let apiResponseRestaurantsRaw
+let apiResponseBooksRaw
+
+let addItemToMoviesList = false;
+let addItemToBooksList = false;
+let addItemToRestaurantsList = false;
+let addItemToProductsList = false;
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     res.render("dashboard");
   });
 
-  router.post("/", (req, res) => {
-    function parseData() {
+  router.post("/", async (req, res) => {
+    function parseMovieData() {
       return new Promise((resolve, reject) => {
         request(`https://www.omdbapi.com/?t=${sanitizeMovieQuery(req.body.movie)}&apikey=a6b04247`, (error, response, body) => {
           if (error) {
@@ -22,42 +30,47 @@ module.exports = (db) => {
         })
       })
     }
-    parseData()
-      .then(body => {
-        if (body.Response === 'False') {
-          return request({
-            url: `https://api.brandfetch.io/v2/brands/${sanitizeFoodBusinessQuery(req.body.movie)}.com`,
-            method: 'GET',
-            headers: { 'Authorization': 'Bearer pAfXYuJYJLCanSk1voTNaoyqJxnYtyGybku75eSeKI0=' },
-          },
-            (error, response, body) => {
-              if (error) {
-                res.send('Please provide a valid search option')
-              } else {
-                return res.send(body)
-              }
-            })
-        } else {
-          return res.send(body)
-        }
-      })
-      .then(body => {
-        if (body.name === "null") {
-          res.send('YES')
-        }
-          // res.send('Yes')
-          // return request(`https://www.googleapis.com/books/v1/volumes?q=${sanitizedBookQuery}&key=AIzaSyADHzbY7CBGfxvALFDuOC6R4OenddipLBM`, (error, response, body) => {
-            // if (error) {
-              // res.send(error)
-            // } else {
-              // res.send(body)
-            // }
-          // })
-      })
-      .catch(error => {
-        res.send('didnt work')
-      })
-  })
+    apiResponseMoviesRaw = await parseMovieData();
 
+    function parseFoodData() {
+      return new Promise((resolve, reject) => {
+        request({
+          url: `https://api.brandfetch.io/v2/brands/${sanitizeFoodBusinessQuery(req.body.movie)}.com`,
+          method: 'GET',
+          headers: { 'Authorization': 'Bearer pAfXYuJYJLCanSk1voTNaoyqJxnYtyGybku75eSeKI0=' },
+        },
+          (error, response, body) => {
+            if (error) {
+              reject(error)
+            } else {
+              return resolve(JSON.parse(body))
+            }
+          })
+      })
+    }
+
+    apiResponseRestaurantsRaw = await parseFoodData();
+
+    function parseBooksData() {
+      return new Promise((resolve, reject) => {
+        request(`https://www.googleapis.com/books/v1/volumes?q=${sanitizeBookAndAuthorQuery(req.body.movie)}&key=AIzaSyADHzbY7CBGfxvALFDuOC6R4OenddipLBM`, (error, response, body) => {
+          if (error) {
+            reject(error)
+          } else {
+            return resolve(JSON.parse(body))
+          }
+        })
+      })
+    }
+    apiResponseBooksRaw = await parseBooksData();
+
+    console.log(apiResponseMoviesRaw);
+    console.log(apiResponseRestaurantsRaw);
+    console.log(apiResponseBooksRaw);
+    console.log(sanitizeMovieQuery(req.body.movie))
+    console.log(sanitizeFoodBusinessQuery(req.body.movie))
+    console.log(sanitizeBookAndAuthorQuery(req.body.movie))
+
+  })
   return router;
-};
+}
