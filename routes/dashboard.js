827422550807ea5ca54284f0e8e9080fd34f7f97
Leeyanhawrt <leeyan1108@gmail.com>
2022-08-02 23:05:23 -0400

    Add header and nav bar changes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -16,23 +16,20 @@ let addItemToProductsList = false;
 module.exports = (db) => {
 
   router.get("/", (req, res) => {
-    // console.log("before db query");
-    // console.log(db);
+    console.log("before db query");
+    console.log(db);
     return db.query(`SELECT title FROM books WHERE id IN (SELECT session.book_id FROM users JOIN session ON users.id = user_id WHERE users.id = 1);`)
     .then(data => {
-      // console.log("after db query");
+      console.log("after db query");
       const tempVar = data.rows;
       console.log(tempVar);
       res.render("dashboard",tempVar);
-        // res.json({ users });
       })
       .catch(err => {
         res
           .status(500)
           .json({ error: err.message });
       });
-
-    // console.log("here");
   });
 
   router.post("/", async (req, res) => {
